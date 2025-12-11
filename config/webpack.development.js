11163ae8f60b277a49fa6a71a8e8413510b97af6
779639338@qq.com <779639338@.com>
2025-12-11 09:55:29 +0800

    fix: 提交favicon.png图标

--- config/webpack.development.js
+++ config/webpack.development.js
@@ -1,5 +1,5 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { resolve, join } = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve, join } = require('path');
 const port = 3000;
 
 module.exports = {
@@ -7,16 +7,16 @@ module.exports = {
     // 单页的spa应用，任意的404请求都返回index.html
     historyApiFallback: true,
     static: {
-      directory: join(__dirname, "../dist")
+      directory: join(__dirname, '../dist'),
     },
     port,
-    hot: true
+    hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      favicon: "./public/favicon.ico",
-      template: resolve(__dirname, "../src/index-dev.html")
-    })
-  ]
+      filename: 'index.html',
+      favicon: './public/favicon.png',
+      template: resolve(__dirname, '../src/index-dev.html'),
+    }),
+  ],
 };
